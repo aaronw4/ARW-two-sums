c911f978e604b4e63e595aafb6625800b1ca83a9
Aaron Williams <aaronrw4@yahoo.com>
2020-10-19 20:06:37 -0700

    reformated code to make it faster

--- two-sums.js
+++ two-sums.js
@@ -1,10 +1,28 @@
 var twoSum = function(nums, target) {
-    for (i = 0; i < nums.length - 1; i++) {
-        for (j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return ([i, j])
-            }
+    // for (i = 0; i < nums.length - 1; i++) {
+    //     for (j = i + 1; j < nums.length; j++) {
+    //         if (nums[i] + nums[j] === target) {
+    //             return ([i, j])
+    //         }
+    //     }
+    // }
+    // return ([])
+
+    // Below code is faster
+
+    object = {}
+    for (i = 0; i < nums.length; i++) {
+        object[nums[i]] = i
+    }
+
+    for (i = 0; i < nums.length; i++) {
+        let number = target - nums[i]
+
+        if (i === object[number]) {
+            continue
+        }
+        else if (number in object) {
+            return ([i, object[number]])
         }
     }
-    return ([])
 };
